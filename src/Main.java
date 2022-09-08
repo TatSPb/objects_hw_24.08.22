93d280fd7daf75c7b07a70989583b8748a3cf699
TatSPb <t-sverchkova@yandex.ru>
2022-09-08 22:20:52 +0300

    Task completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String author1 = String.valueOf(new Author ("Иван", "Первый"));
+        var author2 = new Author ("Игорь", "Второй");
+        var author3 = new Author ("Елена", "Третья");
+
+        //Author newAuthor = new Author ("Иван", "Петров");
+        var newBook1 = new Book("Про 2000й", author1, 2000);
+        var newBook2 = new Book("Про 2010й", author1, 2010);
+        var newBook3 = new Book("Про 2020й", author1, 2020);
+
+        System.out.println("Книга 1: " + newBook1.getName() + " " + newBook1.getAuthor() + " " + newBook1.getPublicationYear());
+        System.out.println("Книга 2: " + newBook2.getName() + " " + newBook2.getAuthor() + " " + newBook2.getPublicationYear());
+        System.out.println("Книга 3: " + newBook3.getName() + " " + newBook3.getAuthor() + " " + newBook3.getPublicationYear());
+        newBook3.setPublicationYear(2021);
+        System.out.println("Книга 3  - новый год издания: " + newBook3.getName() + " " + newBook3.getAuthor() + " " + newBook3.getPublicationYear());
     }
 }
+        //Person sarah = new Person ("Sarah",30); // вызов конструктора. Возраст (int age) БЕЗ КАВЫЧЕК!!! Конструктор - это метод, который создает объект
+       // System.out.println("sarah.name = " + sarah.getName());
+        //System.out.println("sarah.age = " + sarah.getAge());
+        //sarah.setAge(31);
+        //System.out.println("sarah.getAge() = " + sarah.getAge());
+
+       // Person john = new Person("John", 13);
